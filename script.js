94d7ae34bf436052f7beebd5b0bd5c7f1eea1b89
ElBrazilian <nathan.schiffmacher@gmail.com>
2022-08-04 22:14:31 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// when ran, this programm should output a messages with random pieces such as XXX
+// I'm feeling XXX today, thanks to you XXX. You are my XXX.
+
+
+const MessageGenerator = {
+    cutenames: [],
+    feelings: [],
+    names: [],
+
+    initialize(){
+
+    },
+    createMessage(){
+        // let message = '';
+        // message += this.getCutename();
+        // message += this.getFeeling();
+        // message += this.getName();
+        // return message;
+    },
+}
+
+
+function main(){
+    console.log('Hello and welcome in MixedMessages !')
+}
