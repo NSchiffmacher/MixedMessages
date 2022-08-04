23cd47e4417b8f5e5ff9ee7a4ccba77dda1c82b7
ElBrazilian <nathan.schiffmacher@gmail.com>
2022-08-04 23:55:31 +0200

    Project done

--- script.js
+++ script.js
@@ -6,41 +6,70 @@ const MessageGenerator = {
     feelings: [],
     names: [],
 
+    initialized: false,
+
     initialize(){
         try {
             const data = fs.readFileSync('cutenames.txt', 'utf8');
-            this.cutenames = data.split('\r\n');
+            this.cutenames = data.split('\r\n').map(e => e.trim().toLowerCase());
           } catch (err) {
             console.error(err);
           }
           
         try {
             const data = fs.readFileSync('feelings.txt', 'utf8');
-            this.feelings = data.split('\r\n');
+            this.feelings = data.split('\r\n').map(e => e.trim().toLowerCase());
           } catch (err) {
             console.error(err);
           }
           
         try {
             const data = fs.readFileSync('names.txt', 'utf8');
-            this.names = data.split('\r\n');
+            this.names = data.split('\r\n').map(e => e.trim().toLowerCase());
           } catch (err) {
             console.error(err);
           }
+
+        this.initialized = true;
           
     },
+    get name (){
+        if(!this.initialized){
+            console.error('MessageGenerator is not initialized')
+        }
+        let index = Math.floor(Math.random() * this.names.length);
+        return this.names[index];
+    },
+    get feeling (){
+        if(!this.initialized){
+            console.error('MessageGenerator is not initialized')
+        }
+        let index = Math.floor(Math.random() * this.feelings.length);
+        return this.feelings[index];
+    },
+    get cutename(){
+        if(!this.initialized){
+            console.error('MessageGenerator is not initialized')
+        }
+        let index = Math.floor(Math.random() * this.cutenames.length);
+        return this.cutenames[index];
+    },
     createMessage(){
-        // test
-        // test2
-
-        //test3
+        if(!this.initialized){
+            this.initialize();
+        }
+        return `I'm feeling ${this.feeling} today, thanks to you ${this.name}! You are my ${this.cutename}.`;
+        
     },
 }
 
 
 function main(){
-    MessageGenerator.initialize()
-    console.log(MessageGenerator)
     console.log('Hello and welcome in MixedMessages !')
+    console.log()
+
+    console.log(MessageGenerator.createMessage());
+    console.log(MessageGenerator.createMessage());
+    console.log(MessageGenerator.createMessage());
 }
 main()
